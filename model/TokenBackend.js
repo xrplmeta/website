f364939956d2ecee017c0aa5719c5ec2468bde22
Mwni <manuel@tigmar.de>
2022-10-15 18:40:31 +0200

    add token browser search and basic pager

--- model/TokenBackend.js
+++ model/TokenBackend.js
@@ -2,10 +2,16 @@ import createSocket from '@xrplkit/socket'
 import { EventEmitter } from '@mwni/events'
 
 
+const tokensPerPage = 100
+
+
 export default ({ node }) => {
 	let events = new EventEmitter()
 	let loading = false
+	let page = 0
+	let nameFilter
 	let list
+	let total
 	let socket
 
 	function require(){
@@ -13,13 +19,20 @@ export default ({ node }) => {
 			socket = createSocket({ url: node })
 	}
 
-	async function loadAndSubscribePage(index){
-		list = await socket.request({
+	async function loadAndSubscribe(){
+		loading = true
+
+		let { tokens, count } = await socket.request({
 			command: 'tokens',
+			name_like: nameFilter,
 			decode_currency: true,
-			limit: 100,
-			offset: index * 100
+			limit: tokensPerPage,
+			offset: page * tokensPerPage
 		})
+
+		list = tokens
+		total = count
+		loading = false
 
 		events.emit('update')
 	}
@@ -35,11 +48,31 @@ export default ({ node }) => {
 			return list
 		},
 
-		async loadPage(index){
-			loading = true
+		get page(){
+			return page
+		},
+
+		get pages(){
+			return Math.ceil(total / tokensPerPage)
+		},
+
+		get nameFilter(){
+			return nameFilter
+		},
+
+		setNameFilter(term){
+			nameFilter = term ? term : undefined
+			loadAndSubscribe()
+		},
+
+		setPage(index){
+			page = index
+			loadAndSubscribe()
+		},
+
+		async load(){
 			require()
-			await loadAndSubscribePage(index)
-			loading = false
+			await loadAndSubscribe()
 		}
 	}
 }
